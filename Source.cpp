1534005ec8fccfbf8c274fcf324bee741c503454
Marco Costa <marcoemanuelcst15@gmail.com>
2025-05-07 12:19:30 +0100

    Inicializar glew, matrizes de visualização (view) e projeção (projection) com glm

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,9 @@
 ﻿#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #pragma comment(lib, "glew32.lib")
 #pragma comment(lib, "glfw3dll.lib")
@@ -41,10 +44,35 @@ int main(void) {
 	}
 
 	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Erro ao inicializar o GLEW!" << std::endl;
+		return -1;
+	}
+
+
 	glfwSetKeyCallback(window, keyCallback);
 
 	double xCursorPos = width * 0.5, yCursorPos = height * 0.5;
 	glfwSetCursorPos(window, xCursorPos, yCursorPos);
+
+	glm::mat4 model = glm::mat4(1.0f); // Identidade (sem transformações ainda)
+
+	glm::mat4 view = glm::lookAt(
+		glm::vec3(0.0f, 3.0f, 5.0f), // posição da câmara
+		glm::vec3(0.0f, 0.0f, 0.0f), // olha para o centro
+		glm::vec3(0.0f, 1.0f, 0.0f)  // vetor "cima"
+	);
+
+	glm::mat4 projection = glm::perspective(
+		glm::radians(45.0f),         // campo de visão
+		(float)width / (float)height, // aspeto
+		0.1f, 100.0f                 // plano próximo e distante
+	);
+
+
+
 
 	// Update
 	while (!glfwWindowShouldClose(window)) {
